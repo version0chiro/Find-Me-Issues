a696af92f93e17b68bb9dad108a259a671354feb
Chiro <sachinsinghroxx@gmail.com>
2021-04-30 15:57:55 +0530

    Added a transition for showing issues

--- client/src/components/SingleCard.js
+++ client/src/components/SingleCard.js
@@ -1,21 +1,34 @@
 import { Card, Button } from "react-bootstrap";
+import { useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
+import { Animate } from "react-show";
+
+import IssuesList from "./IssuesList.js"
 
 const SingleCard = (props) => {
+  const [openIssues, setOpen] = useState(false);
   return (
-    <div>
-      <Card style={{ width: "100%", margin: "10px",padding: "10px"}}>
+    <div style={{ width: "100%", margin: "10px", padding: "10px" }}>
+      <Card>
         <Card.Body>
-          <div style={{display: "inline-block"}}>
-            <Avatar  style={{display: "inline-block"}} src={props.repo.owner.avatar_url} />
-            <Card.Title >{props.repo.name}</Card.Title>
+          <div style={{ display: "inline-block" }}>
+            <Avatar
+              style={{ display: "inline-block" }}
+              src={props.repo.owner.avatar_url}
+            />
+            <Card.Title>{props.repo.name}</Card.Title>
           </div>
           <Card.Text>{props.repo.description}</Card.Text>
-          <Card.Text>Total Number of Issues {props.repo.open_issues_count}</Card.Text>
+          <Card.Text>
+            Total Number of Issues {props.repo.open_issues_count}
+          </Card.Text>
           <Card.Text> Language: {props.repo.language}</Card.Text>
-          <a href={props.repo.html_url}>
-            <Button variant="primary">Go To Issues</Button>
-          </a>
+          {/* <a href={props.repo.html_url}> */}
+          <Button variant="primary" onClick={() => setOpen(!openIssues)}>
+            Go To Issues
+          </Button>
+          {/* </a> */}
+          <IssuesList openIssues={openIssues}/>
         </Card.Body>
       </Card>
     </div>
