e2f6a6498e4b6f585f6ce91f2d624d5ec0e62702
Carrum <calumchansy@gmail.com>
2024-05-07 01:27:34 -0700

    New Navbar Kinda

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState } from "react";
 
-import './App.css'
+import "./App.css";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 //Context
-import { ThemeContext } from './Context/themeContext'
+import { ThemeContext } from "./Context/themeContext";
 //Components
-import Header from './components/Header'
-import CardSet from './components/CardSet'
-import Navigation from './components/Navigation'
-import RepoFilters from './components/RepoFilters'
+import Header from "./components/Header";
+import CardSet from "./components/CardSet";
+import Navigation from "./components/Navigation";
+import RepoFilters from "./components/RepoFilters";
 
-function App () {
-  const [language, setLanguage] = useState('Javascript')
-  const [pageNumber, setPageNumber] = useState(1)
-  const [maxPageNumber, setMaxPageNumber] = useState(100)
-  const [inputSearch, setInputSearch] = useState('')
-  const [sortByForks, setSortByForks] = useState('desc')
-  const [sortByStars, setSortByStars] = useState('desc')
+function App() {
+  const [language, setLanguage] = useState("Javascript");
+  const [pageNumber, setPageNumber] = useState(1);
+  const [maxPageNumber, setMaxPageNumber] = useState(100);
+  const [inputSearch, setInputSearch] = useState("");
+  const [sortByForks, setSortByForks] = useState("desc");
+  const [sortByStars, setSortByStars] = useState("desc");
   const [reducedState, setReducedState] = useState({
-    minForks: '',
-    maxForks: '',
-    minStars: '',
-    maxStars: ''
-  })
-  const [hidePagination, setHidePagination] = useState(true)
+    minForks: "",
+    maxForks: "",
+    minStars: "",
+    maxStars: "",
+  });
+  const [hidePagination, setHidePagination] = useState(true);
 
-  const { theme } = useContext(ThemeContext)
+  const { theme } = useContext(ThemeContext);
 
   return (
     <div
-      className='App'
+      className="App"
       style={{ backgroundColor: theme.bg, color: theme.color }}
     >
       <Header
+        language={language}
         setLanguage={setLanguage}
         setInputSearch={setInputSearch}
         inputSearch={inputSearch}
@@ -74,7 +75,7 @@ function App () {
         hidePagination={hidePagination}
       />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,13 @@
-import { Navbar, Nav, NavDropdown, Container, Button } from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Container,
+  Row,
+  Button,
+} from "react-bootstrap";
+// import FormSelect from "react-bootstrap/FormSelect";
+import Form from "react-bootstrap/Form";
 import { useContext, useState } from "react";
 import langugagesData from "../data/languages.json";
 import { useDebouncedCallback } from "use-debounce";
@@ -6,99 +15,130 @@ import { useDebouncedCallback } from "use-debounce";
 import { ThemeContext } from "../Context/themeContext";
 
 const Header = (props) => {
-	const [language, setLanguage] = useState("javascript");
-	const { theme, changeTheme } = useContext(ThemeContext);
-	const [inputSearch, setInputSearch] = useState("");
+  //   const [language, setLanguage] = useState("Javascript");
+  const { theme, changeTheme } = useContext(ThemeContext);
+  const [inputSearch, setInputSearch] = useState("");
 
-	const debouncedInput = useDebouncedCallback(
-		(value) => {
-			props.setInputSearch(value);
-		},
-		// delay in ms
-		1000
-	);
+  const debouncedInput = useDebouncedCallback(
+    (value) => {
+      props.setInputSearch(value);
+    },
+    // delay in ms
+    1000
+  );
 
-	const handleInputSearch = (inputValue) => {
-		setInputSearch(inputValue);
-		debouncedInput(inputValue);
-	};
+  const handleInputSearch = (inputValue) => {
+    setInputSearch(inputValue);
+    debouncedInput(inputValue);
+  };
 
-	const handleSortByStars = () => {
-		props.setSortByForks("");
-		if (props.sortByStars === "desc") props.setSortByStars("asc");
-		else props.setSortByStars("desc");
-	};
-	const handleSortByForks = () => {
-		props.setSortByStars("");
-		if (props.sortByForks === "desc") props.setSortByForks("asc");
-		else props.setSortByForks("desc");
-	};
+  const handleSortByStars = () => {
+    props.setSortByForks("");
+    if (props.sortByStars === "desc") props.setSortByStars("asc");
+    else props.setSortByStars("desc");
+  };
+  const handleSortByForks = () => {
+    props.setSortByStars("");
+    if (props.sortByForks === "desc") props.setSortByForks("asc");
+    else props.setSortByForks("desc");
+  };
 
-	return (
-			<Navbar bg={theme.mode} variant={theme.mode} expand="lg" id="header">
-				<Container fluid>
-					<Navbar.Brand href="#home">Find Me Issues</Navbar.Brand>
-					{theme.mode === "light" ? (
-						<Button onClick={changeTheme} size="sm">
-							<i className="fa fa-moon-o" aria-hidden="true"></i>
-						</Button>
-					) : (
-						<Button onClick={changeTheme} size="sm">
-							<i className="fa fa-sun-o" aria-hidden="true"></i>
-						</Button>
-					)}
-					<Nav className="mr-auto"></Nav>
+  return (
+    <Navbar
+      bg={theme.mode}
+      variant={theme.mode}
+      id="header"
+      style={{ padding: ".5rem" }}
+    >
+      <Container fluid>
+        <Navbar.Brand href="#home">Find Me Issues</Navbar.Brand>
+        {/* <Button
+          size="sm"
+          style={{
+            margin: "0px 3px",
+          }}
+          onClick={handleSortByStars}
+        >
+          Sort by stars
+        </Button>
+        <Button
+          size="sm"
+          style={{
+            margin: "0px 3px",
+          }}
+          onClick={handleSortByForks}
+        >
+          Sort by forks
+        </Button> */}
+        {/* <span className="ml-2 mr-1">
+          Find specific content in the project description:{" "}
+        </span> */}
+        <Container style={{ width: "50%", padding: "0", margin: "0" }}>
+          <Row style={{ padding: "0", margin: "0" }}>
+            <input
+              type="text"
+              value={inputSearch}
+              placeholder="Search Project Descriptions..."
+              onChange={(e) => handleInputSearch(e.target.value)}
+              style={{
+                width: "50%",
+              }}
+            />
+            <Form.Control
+              as="select"
+              title={props.language}
+              id="basic-nav-dropdown"
+              style={{
+                width: "50%",
+                padding: "2px",
+              }}
+            >
+              {langugagesData.languages.map((lang, index) => {
+                return (
+                  <option
+                    key={index}
+                    onClick={() => {
+                      //   setLanguage(lang);
+                      props.setLanguage(lang);
+                    }}
+                    selected={lang === props.language ? true : false}
+                  >
+                    {lang}
+                  </option>
+                );
+              })}
+            </Form.Control>
+          </Row>
+        </Container>
 
-					<Button size="sm"
-						style={{
-							margin: "0px 3px",
-						}}
-						onClick={handleSortByStars}
-					>
-						Sort by stars
-					</Button>
-					<Button size="sm"
-						style={{
-							margin: "0px 3px",
-						}}
-						onClick={handleSortByForks}
-					>
-						Sort by forks
-					</Button>
-
-
-					<span className="ml-2 mr-1">Find specific content in the project description: </span>
-					<input
-						type="text"
-						value={inputSearch}
-						onChange={(e) => handleInputSearch(e.target.value)}
-					/>
-					<div id="outlined-basic" className="mr-sm-2">
-						{language}
-					</div>
-					<NavDropdown title="Select Language" id="basic-nav-dropdown">
-						<div style={{ height: "400px", overflowY: "auto" }}>
-							{langugagesData.languages.map((lang, index) => {
-								return (
-									<div key={index}>
-										<NavDropdown.Item
-											onClick={() => {
-												setLanguage(lang);
-												props.setLanguage(lang);
-											}}
-										>
-											{lang}
-										</NavDropdown.Item>
-										<NavDropdown.Divider />
-									</div>
-								);
-							})}
-						</div>
-					</NavDropdown>
-				</Container>
-
-			</Navbar>
-	);
+        {/* <NavDropdown title={language} id="basic-nav-dropdown">
+          <div style={{ height: "400px", overflowY: "auto" }}>
+            {langugagesData.languages.map((lang, index) => {
+              return (
+                <div key={index}>
+                  <NavDropdown.Item
+                    onClick={() => {
+                      setLanguage(lang);
+                      props.setLanguage(lang);
+                    }}
+                  >
+                    {lang}
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                </div>
+              );
+            })}
+          </div>
+        </NavDropdown> */}
+        <Button onClick={changeTheme} size="sm">
+          <i
+            className={theme.mode === "light" ? "fa fa-moon-o" : "fa fa-sun-o"}
+            aria-hidden="true"
+          />
+        </Button>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
